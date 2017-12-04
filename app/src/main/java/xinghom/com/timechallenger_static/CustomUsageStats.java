d21b17411f8426128dfedf77fb617209265dd6a1
Xinghang Xu <xinghom@Xinghangs-MacBook-Pro.local>
2017-12-03 23:11:43 -0800

    interim demo

---
+++ app/src/main/java/xinghom/com/timechallenger_static/CustomUsageStats.java
@@ -0,0 +1,16 @@
+package xinghom.com.timechallenger_static;
+
+/**
+ * Created by xinghom on 11/11/17.
+ */
+
+import android.app.usage.UsageStats;
+import android.graphics.drawable.Drawable;
+
+/**
+ * Entity class represents usage stats and app icon.
+ */
+public class CustomUsageStats {
+    public UsageStats usageStats;
+    public Drawable appIcon;
+}
---
+++ app/src/main/java/xinghom/com/timechallenger_static/MainActivity.java
@@ -0,0 +1,19 @@
+package xinghom.com.timechallenger_static;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        if (savedInstanceState == null) {
+            getFragmentManager().beginTransaction()
+                    .add(R.id.container, AppUsageStatisticsFragment.newInstance())
+                    .commit();
+        }
+    }
+}
+
