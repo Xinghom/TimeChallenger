eec71391744b3538f33062abb7c52e8e03159866
Xinghang Xu <xinghom@eduroam-169-233-233-60.ucsc.edu>
2017-12-06 15:17:02 -0800

    menu

--- app/src/main/java/xinghom/com/timechallenger_static/MainActivity.java
+++ app/src/main/java/xinghom/com/timechallenger_static/MainActivity.java
@@ -2,6 +2,9 @@ package xinghom.com.timechallenger_static;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +18,23 @@ public class MainActivity extends AppCompatActivity {
                     .commit();
         }
     }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater(). inflate( R.menu.main_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.statisticsItem: Toast.makeText( this, "Statistics", Toast.LENGTH_SHORT). show();
+                break;
+            case R.id.challengeModeItem: Toast.makeText( this, "ChallengeMode", Toast.LENGTH_SHORT). show();
+                break;
+            default:
+        }
+        return true;
+    }
+
+
 }
 
