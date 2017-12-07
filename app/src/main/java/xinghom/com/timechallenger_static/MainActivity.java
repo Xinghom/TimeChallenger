a8cdb5c473b2c43e725f95c3d26ef82aaaaf45fa
陈月婷 <chenyueting@eduroam-169-233-210-2.ucsc.edu>
2017-12-06 23:20:33 -0800

    cyt_done_menu

--- app/src/main/java/xinghom/com/timechallenger_static/MainActivity.java
+++ app/src/main/java/xinghom/com/timechallenger_static/MainActivity.java
@@ -1,22 +1,45 @@
 package xinghom.com.timechallenger_static;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        /*控制侧滑菜单*/
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        /*设定NavigationView菜单的选择事件*/
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
         if (savedInstanceState == null) {
             getFragmentManager().beginTransaction()
                     .add(R.id.container, AppUsageStatisticsFragment.newInstance())
                     .commit();
         }
+
     }
 
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -35,6 +58,38 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
+    @SuppressWarnings("StatementWithEmptyBody")
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            Intent intent= new Intent(MainActivity.this,Activity2.class);
+            startActivity(intent);
+
+        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_share) {
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        /*关闭侧滑菜单*/
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
 }
+
+
+
+
 
